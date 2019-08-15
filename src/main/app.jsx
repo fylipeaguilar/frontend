4ecf4ac0da663362316cbcd5c96c8e1c392ba1a4
fylipeaguilar <50252863+fylipeaguilar@users.noreply.github.com>
2019-08-15 10:29:47 -0300

    Falha ao rodar a aplicação
    ERROR in ./src/index.jsx
    Module parse failed: C:\Users\fylip\Google Drive\Fylipe_Pessoal\Cursos_Aprendizados\Udemy\React_Redux\todo_app\frontend\src\index.jsx Unexpected token (5:16)
    You may need an appropriate loader to handle this file type.
    SyntaxError: Unexpected token (5:16)
        at Parser.pp$4.raise (C:\Users\fylip\Google Drive\Fylipe_Pessoal\Cursos_Aprendizados\Udemy\React_Redux\todo_app\frontend\node_modules\acorn\dist\acorn.js:2221:15)
        at Parser.pp.unexpected (C:\Users\fylip\Google Drive\Fylipe_Pessoal\Cursos_Aprendizados\Udemy\React_Redux\todo_app\frontend\node_modules\acorn\dist\acorn.js:603:10)
        at Parser.pp$3.parseExprAtom (C:\Users\fylip\Google Drive\Fylipe_Pessoal\Cursos_Aprendizados\Udemy\React_Redux\todo_app\frontend\node_modules\acorn\dist\acorn.js:1822:12)
        at Parser.pp$3.parseExprSubscripts (C:\Users\fylip\Google Drive\Fylipe_Pessoal\Cursos_Aprendizados\Udemy\React_Redux\todo_app\frontend\node_modules\acorn\dist\acorn.js:1715:21)
        at Parser.pp$3.parseMaybeUnary (C:\Users\fylip\Google Drive\Fylipe_Pessoal\Cursos_Aprendizados\Udemy\React_Redux\todo_app\frontend\node_modules\acorn\dist\acorn.js:1692:19)
        at Parser.pp$3.parseExprOps (C:\Users\fylip\Google Drive\Fylipe_Pessoal\Cursos_Aprendizados\Udemy\React_Redux\todo_app\frontend\node_modules\acorn\dist\acorn.js:1637:21)
        at Parser.pp$3.parseMaybeConditional (C:\Users\fylip\Google Drive\Fylipe_Pessoal\Cursos_Aprendizados\Udemy\React_Redux\todo_app\frontend\node_modules\acorn\dist\acorn.js:1620:21)
        at Parser.pp$3.parseMaybeAssign (C:\Users\fylip\Google Drive\Fylipe_Pessoal\Cursos_Aprendizados\Udemy\React_Redux\todo_app\frontend\node_modules\acorn\dist\acorn.js:1597:21)
        at Parser.pp$3.parseExprList (C:\Users\fylip\Google Drive\Fylipe_Pessoal\Cursos_Aprendizados\Udemy\React_Redux\todo_app\frontend\node_modules\acorn\dist\acorn.js:2165:22)
        at Parser.pp$3.parseSubscripts (C:\Users\fylip\Google Drive\Fylipe_Pessoal\Cursos_Aprendizados\Udemy\React_Redux\todo_app\frontend\node_modules\acorn\dist\acorn.js:1741:35)
     @ multi main

---
+++ src/main/app.jsx
@@ -0,0 +1,10 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'module/font-awesome/css/font-awesome.mim.css'
+
+import React from 'react'
+
+export default props => (
+    <div className='container'>
+        <h1>Teste</h1>
+    </div>
+)
